9625b2ce73b6b84b7f00175cfbab21ff6ec3c389
Antonio Griffith-Keaton <antoniogriffith2@yahoo.com>
2021-03-08 01:36:34 -0500

    Data Validation Complete
    User is required to enter an alphanumeric string containing less than five characters. Program also rejects stock symbols not identified by ALPHAVANTAGE"S API.

--- App/robo_advisor.py
+++ App/robo_advisor.py
@@ -4,12 +4,12 @@
 import os
 import requests
 import json
+import datetime
 from dotenv import load_dotenv
 
+# Accessing Secret API Key Value
 load_dotenv()
-print(os.getenv("MY_SECRET_MESSAGE"))
-
-quit()
+api_key = os.getenv("ALPHAVANTAGE_API_KEY")
 
 
 # utility function to convert float or integer to USD-formatted string (for printing)
@@ -22,20 +22,31 @@ def to_usd(my_price):
 # INFO INPUTS
 #
 
+time_of_request = datetime.datetime.now().strftime("%Y-%m-%d %I:%M:%S %p")
 
-request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo"
+while True:
+    symbol = input("Please enter the correct stock ticker of a company to receive a recommendation: ")
 
-response = requests.get(request_url)
-# print(type(response)) #> 'requests.models.Response'>
-# print(response.status_code) #> 200
-# print(response.text)
+    if (len(symbol) > 5 or  symbol.isalpha() == False):
+        print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.\n")
+    else:
+        request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=" + symbol + "&apikey=" + api_key
 
-parsed_response = json.loads(response.text)
+        response = requests.get(request_url)
+        # print(type(response)) #> 'requests.models.Response'>
+        # print(response.status_code) #> 200
+        # print(response.text)
+
+        parsed_response = json.loads(response.text)
+
+        errorCheck = list(parsed_response.keys())
+
+        if ("Error Message" in errorCheck):
+            print("Sorry, couldn't find any trading data for that stock symbol. Please try again. \n")
+        else:
+            break
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
-
-# breakpoint()
 
 tsd = parsed_response['Time Series (Daily)']
 
@@ -61,18 +72,24 @@ recent_high = max(high_prices)
 # manimum of all low prices
 recent_low = min(low_prices)
 
+
+
+
+
+
+
+
+
+
 #
 # INFO OUTPUTS
 #
 
-
-
-
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm") #> using datetime module
+print(f"REQUEST AT: {time_of_request}") #> using datetime module
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
